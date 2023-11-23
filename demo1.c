22db1d11d028f549e8ab0a4ad0286c623856e303
Joshua <joshua537.nit@gmail.com>
2023-11-23 15:14:17 +0530

    Add files via upload

---
+++ demo1.c
@@ -0,0 +1,13 @@
+//%compiler: clang
+//%cflags: -fopenmp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]){
+    #pragma omp parallel
+    printf("%s\n", "Hello World");
+
+    return 0;
+}
