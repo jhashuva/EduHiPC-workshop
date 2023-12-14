764c4801d24f685eb585d0174ff6203796bfb7e8
Joshua <joshua537.nit@gmail.com>
2023-12-14 15:04:18 +0530

    Update OpenMP_Prime_Numbers.c

--- assignments/Session-3_Self_Explonatory_Problem/OpenMP_Prime_Numbers.c
+++ assignments/Session-3_Self_Explonatory_Problem/OpenMP_Prime_Numbers.c
@@ -12,3 +12,54 @@ the provided **`isprime`** function.
 4. The program should be able to handle different values of **`NUM`** for experimentation.
 5. Print the total number of prime numbers found within the specified range.
 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+void findPrimes(int n, int **arr) {
+    int count = 0;
+    #pragma omp parallel for
+    for (int i = 2; i <= n; i++) {
+        int flag = 1;
+        for (int j = 2; j <= i / 2+1; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            //printf("%d\n", i);
+            (*arr)[count++] = i; // Storing prime number in the array
+        }
+
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int)); // Dynamic memory allocation
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+          findPrimes(n, &arr);
+        }
+
+    }
+
+
+    // Optional: Print the primes stored in the array
+    printf("Primes up to %d:\n", n);
+    for (int i = 0; arr[i] != 0 && i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr); //free dynamically allocated memory
+
+    return 0;
+}
